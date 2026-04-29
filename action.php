2e7a70a0ebaac04aff0f9b8958dc611611ea645e
pollito <nickdate@yahoo.co.uk>
2026-04-29 23:09:36 +0300

    Remove redundant require_once for action.php
    Removed "require_once(DOKU_PLUGIN.'action.php');" as it is deprecated, and was causing the "deprecated" log to fill up.

--- action.php
+++ action.php
@@ -13,8 +13,6 @@ if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
 if(!defined('DOKU_LF')) define('DOKU_LF', "\n");
 
-require_once(DOKU_PLUGIN.'action.php');
-
 /**
  * All DokuWiki plugins to interfere with the event system
  * need to inherit from this class
